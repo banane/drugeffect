5aaa251b93ef11b77826fe98720f420ba5d73d86
banane <banane@gmail.com>
2025-08-22 13:29:33 -0700

    Removed db tables

--- db/migrate/20250822182800_create_adverse_effects.rb
+++
@@ -1,9 +0,0 @@
-class CreateAdverseEffects < ActiveRecord::Migration[7.1]
-  def change
-    create_table :adverse_effects do |t|
-      t.string :effect
-
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,5 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_08_22_182800) do
-  create_table "adverse_effects", force: :cascade do |t|
-    t.string "effect"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
+ActiveRecord::Schema[7.1].define(version: 0) do
 end
